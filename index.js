297bbaee4d3e62b6c5be0f44c3afff24870f5a63
Alex Denton <alex.jdenton@gmail.com>
2018-01-18 00:12:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function getFirstSelector(selector) {
+  var firstElement = document.querySelector(selector);
+  return firstElement;
+}
+function nestedTarget () {
+  //var elementId = document.getElementById('nested');
+
+  //var nestedElement = document.querySelectorAll(elementId.target);
+  //return nestedElement;
+
+  return document.getElementById('nested')
+}
